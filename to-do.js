c65ddec13ef427bc318dbc286d22ca9d39e0a513
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-28 23:20:39 +0530

    added a simple test

--- to-do.js
+++ to-do.js
@@ -1,1 +1,14 @@
-const form_text = 
+const form_input = document.getElementById("form_input");
+const input_text = document.getElementById("input_text");
+const button_clear = document.getElementById("button_clear");
+input_text.focus();
+
+form_input.addEventListener("submit", event => {
+    event.preventDefault();
+    if(input_text.value != "") {
+        alert(input_text.value);
+
+        input_text.value = "";
+        input_text.focus();
+    }
+})
